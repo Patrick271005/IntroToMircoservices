9250c8e740ee769ee36a5d162e1f0cca5be4b3b8
PATRICKFLANAGAN-STUD <G00431255@atu.ie>
2025-09-26 09:56:15 +0100

    used Pathvariable method

--- src/main/java/ie/atu/lab1a/RequestController.java
+++ src/main/java/ie/atu/lab1a/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.lab1a;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,11 @@ public class RequestController {
     public String helloAlso()
     {
         return "Hello";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable("name") String name)
+    {
+        return "Hello " + name + "!";
     }
 }
