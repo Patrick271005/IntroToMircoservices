765128b116e00e4cc647a051210b3ed04c5d745a
PATRICKFLANAGAN-STUD <G00431255@atu.ie>
2025-09-26 09:20:39 +0100

    Created Project

---
+++ src/main/java/ie/atu/lab1a/Lab1AApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab1a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1AApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1AApplication.class, args);
+    }
+
+}
